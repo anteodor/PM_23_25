98be0f7fa35b54b25d1642cb53b2216aa5ca4e3d
Anastasia Teodorovska <anastasiia.teodorovska.pm.2023@lpnu.ua>
2024-11-10 16:36:28 +0200

    almost the end of the development of the css file

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJSON("../data/data.json", function(data) {
+    $.getJSON("data/data.json", function(data) {
         // Load Name and Position
         $('.profile-name').text(data.name);
         $('.profile-surname').text(data.surname);
@@ -12,32 +12,42 @@ $(document).ready(function() {
         // Contact Information
          const contact = data.about_me.contact;
          $('.contact-container').append(
-            `<li>${contact.phone}</li>
-            <li>${contact.website}</li>
-            <li>${contact.address}</li>`
+            `<li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${contact.phone}</li>
+            <li><i class="bi bi-globe"></i>${contact.website}</li>
+            <li><i class="bi bi-geo-alt-fill"></i>${contact.address}</li>`
         );   
-        
+
         // References
         data.references.forEach(ref => {
           $('.references-container').append(
-            `<div>
-              <h5>${ref.name}</h5>
-              <p>${ref.address}</p>
-              <p>Tel: ${ref.phone}</p>
-              <p>Email: ${ref.email}</p>
+            `<div class="container-item">
+              <div class="line"></div>
+              <div class="square"></div>
+              <div class="container-information" style="line-height: 10px;">
+                <h4 class="degree-text">${ref.name}</h4>
+                <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${ref.address}</span></p>
+                <p class="description">Tel: ${ref.phone}</p>
+                <p class="description">Email: ${ref.email}</p>
+              </div> 
             </div>`
           );
         });   
-        
+
         // Job Experience
         data.job_experience.forEach(job => {
           $('.job-container').append(
-            `<div>
-              <p>${job.company}</p>
-              <p>${job.location}</p>
-              <h5>${job.position}</h5>
-              <p>${job.dates}</p>
-              <p>${job.description}</p>
+            `<div class="container-item">
+               <div class="line"></div>
+               <div class="square"></div>
+                <div class="container-information">
+                 <p class="place-text">${job.company}</p>
+                 <p class="location-text" style="margin-top:-18px">${job.location}</p>
+                  <div class="date_location" style="margin-top:-22px">
+                   <h4 class="degree-text">${job.position}</h4>
+                   <p class="dates">${job.dates}</p>
+                  </div> 
+                 <p class="description" style="margin-top:-15px">${job.description}</p>
+                </div>
             </div>`
           );
         });   
@@ -45,11 +55,16 @@ $(document).ready(function() {
         // Education
         data.education.forEach(edu => {
           $('.education-container').append(
-            `<div>
-              <p>${edu.institution}</p>
-              <h5>${edu.degree}</h5>
-              <p>${edu.dates}</p>
-              <p>${edu.description}</p>
+            `<div class="container-item">
+               <div class="line"></div>
+               <div class="square"></div>
+                <div class="container-information">
+                  <p class="place-text">${edu.institution}</p>
+                  <h4 class="degree-text" style="margin-top: -16px">${edu.degree}</h4>
+                  <p class="dates">${edu.dates}</p>
+                  <div class="container-main-information">
+                  <p class="description">${edu.description}</p>
+                </div>
             </div>`
           );
         });
@@ -57,9 +72,11 @@ $(document).ready(function() {
         // Skills
         data.skills.forEach(skill => {
           $('.skills-container').append(
-            `<div class="d-flex justify-content-between">
-              <span>${skill.name}</span>
-              <span>${skill.score}</span>
+            `<div class="skill">
+              <p class="skill-text">${skill.name}</p>
+              <div class="skill-bar">
+                <div class="skill-score" style="width: ${skill.score}"></div>
+              </div>
             </div>`
           );
         });  
@@ -67,21 +84,33 @@ $(document).ready(function() {
         // Hobbies
         data.hobbies.forEach(hobby => {
           $('.hobbies-container').append(
-            `<div class="d-flex justify-content-between">
-              <span>${hobby.name}</span>
-              <span>${hobby.score}</span>
+            `<div class="skill">
+              <p class="skill-text">${hobby.name}</p>
+              <div class="skill-bar">
+                <div class="skill-score" style="width: ${hobby.score}"></div>
+              </div>
             </div>`
           );
-        });    
-        
+        });   
+
         // Languages
         data.languages.forEach(language => {
-          $('.languages-container').append(
-            `<div class="d-flex justify-content-between">
-              <span>${language.name}</span>
-              <span>${language.score}</span>
-            </div>`
-          );
+          const score = parseInt(language.score);
+          const progressBarHTML = `
+            <div class="box">
+              <div class="percent">
+                <svg>
+                  <circle cx="40" cy="40" r="40"></circle>
+                  <circle cx="40" cy="40" r="40" style="stroke-dashoffset: calc(251 - (251 * ${score}) / 100);"></circle>
+                </svg>
+                <div class="num">
+                  <h1>${score}<span>%</span></h1>
+                  <h2 class="text">${language.name}</h2>
+                </div>
+              </div>
+            </div>
+          `;
+          $('.languages-container').append(progressBarHTML);
         });
       });
     });
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,28 +1,57 @@
-$(document).ready(function(){$.getJSON("../data/data.json",function(e){$(".profile-name").text(e.name),$(".profile-surname").text(e.surname),$(".position-title").text(e.position),$(".about-main-text").text(e.about_me.text),$(".about-quote-text").text(e.about_me.quote);var n=e.about_me.contact;$(".contact-container").append(`<li>${n.phone}</li>
-            <li>${n.website}</li>
-            <li>${n.address}</li>`),e.references.forEach(e=>{$(".references-container").append(`<div>
-              <h5>${e.name}</h5>
-              <p>${e.address}</p>
-              <p>Tel: ${e.phone}</p>
-              <p>Email: ${e.email}</p>
-            </div>`)}),e.job_experience.forEach(e=>{$(".job-container").append(`<div>
-              <p>${e.company}</p>
-              <p>${e.location}</p>
-              <h5>${e.position}</h5>
-              <p>${e.dates}</p>
-              <p>${e.description}</p>
-            </div>`)}),e.education.forEach(e=>{$(".education-container").append(`<div>
-              <p>${e.institution}</p>
-              <h5>${e.degree}</h5>
-              <p>${e.dates}</p>
-              <p>${e.description}</p>
-            </div>`)}),e.skills.forEach(e=>{$(".skills-container").append(`<div class="d-flex justify-content-between">
-              <span>${e.name}</span>
-              <span>${e.score}</span>
-            </div>`)}),e.hobbies.forEach(e=>{$(".hobbies-container").append(`<div class="d-flex justify-content-between">
-              <span>${e.name}</span>
-              <span>${e.score}</span>
-            </div>`)}),e.languages.forEach(e=>{$(".languages-container").append(`<div class="d-flex justify-content-between">
-              <span>${e.name}</span>
-              <span>${e.score}</span>
-            </div>`)})})});
+$(document).ready(function(){$.getJSON("data/data.json",function(i){$(".profile-name").text(i.name),$(".profile-surname").text(i.surname),$(".position-title").text(i.position),$(".about-main-text").text(i.about_me.text),$(".about-quote-text").text(i.about_me.quote);var e=i.about_me.contact;$(".contact-container").append(`<li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${e.phone}</li>
+            <li><i class="bi bi-globe"></i>${e.website}</li>
+            <li><i class="bi bi-geo-alt-fill"></i>${e.address}</li>`),i.references.forEach(i=>{$(".references-container").append(`<div class="container-item">
+              <div class="line"></div>
+              <div class="square"></div>
+              <div class="container-information" style="line-height: 10px;">
+                <h4 class="degree-text">${i.name}</h4>
+                <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${i.address}</span></p>
+                <p class="description">Tel: ${i.phone}</p>
+                <p class="description">Email: ${i.email}</p>
+              </div> 
+            </div>`)}),i.job_experience.forEach(i=>{$(".job-container").append(`<div class="container-item">
+               <div class="line"></div>
+               <div class="square"></div>
+                <div class="container-information">
+                 <p class="place-text">${i.company}</p>
+                 <p class="location-text" style="margin-top:-18px">${i.location}</p>
+                  <div class="date_location" style="margin-top:-22px">
+                   <h4 class="degree-text">${i.position}</h4>
+                   <p class="dates">${i.dates}</p>
+                  </div> 
+                 <p class="description" style="margin-top:-15px">${i.description}</p>
+                </div>
+            </div>`)}),i.education.forEach(i=>{$(".education-container").append(`<div class="container-item">
+               <div class="line"></div>
+               <div class="square"></div>
+                <div class="container-information">
+                  <p class="place-text">${i.institution}</p>
+                  <h4 class="degree-text" style="margin-top: -16px">${i.degree}</h4>
+                  <p class="dates">${i.dates}</p>
+                  <div class="container-main-information">
+                  <p class="description">${i.description}</p>
+                </div>
+            </div>`)}),i.skills.forEach(i=>{$(".skills-container").append(`<div class="skill">
+              <p class="skill-text">${i.name}</p>
+              <div class="skill-bar">
+                <div class="skill-score" style="width: ${i.score}"></div>
+              </div>
+            </div>`)}),i.hobbies.forEach(i=>{$(".hobbies-container").append(`<div class="skill">
+              <p class="skill-text">${i.name}</p>
+              <div class="skill-bar">
+                <div class="skill-score" style="width: ${i.score}"></div>
+              </div>
+            </div>`)}),i.languages.forEach(i=>{var e=parseInt(i.score),e=`
+            <div class="box">
+              <div class="percent">
+                <svg>
+                  <circle cx="40" cy="40" r="40"></circle>
+                  <circle cx="40" cy="40" r="40" style="stroke-dashoffset: calc(251 - (251 * ${e}) / 100);"></circle>
+                </svg>
+                <div class="num">
+                  <h1>${e}<span>%</span></h1>
+                  <h2 class="text">${i.name}</h2>
+                </div>
+              </div>
+            </div>
+          `;$(".languages-container").append(e)})})});

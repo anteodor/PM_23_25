ce6d340ea2fba5a37670c7bfaf59f573952731cd
Anastasia Teodorovska <anastasiia.teodorovska.pm.2023@lpnu.ua>
2024-11-15 16:49:19 +0200

    developing the behaviour of web page components in javascript and adding asynchronous HTTP requests to script.js

--- app/js/script.js
+++ app/js/script.js
@@ -1,117 +1,230 @@
-$(document).ready(function() {
-    $.getJSON("data/data.json", function(data) {
-        // Load Name and Position
-        $('.profile-name').text(data.name);
-        $('.profile-surname').text(data.surname);
-        $('.position-title').text(data.position);
-    
-        // Load About Me
-        $('.about-main-text').text(data.about_me.text);
-        $('.about-quote-text').text(data.about_me.quote);
- 
-        // Contact Information
-         const contact = data.about_me.contact;
-         $('.contact-container').append(
-            `<li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${contact.phone}</li>
-            <li><i class="bi bi-globe"></i>${contact.website}</li>
-            <li><i class="bi bi-geo-alt-fill"></i>${contact.address}</li>`
-        );   
+document.addEventListener("DOMContentLoaded", function() {
+  async function loadDataWithFetch() {
+    try {
+      const response = await fetch("data/data.json");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
 
-        // References
-        data.references.forEach(ref => {
-          $('.references-container').append(
-            `<div class="container-item">
-              <div class="line"></div>
-              <div class="square"></div>
-              <div class="container-information" style="line-height: 10px;">
-                <h4 class="degree-text">${ref.name}</h4>
-                <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${ref.address}</span></p>
-                <p class="description">Tel: ${ref.phone}</p>
-                <p class="description">Email: ${ref.email}</p>
-              </div> 
-            </div>`
-          );
-        });   
+      // Load Name and Position
+      document.querySelector('.profile-name').textContent = data.name;
+      document.querySelector('.profile-surname').textContent = data.surname;
+      document.querySelector('.position-title').textContent = data.position;
 
-        // Job Experience
-        data.job_experience.forEach(job => {
-          $('.job-container').append(
-            `<div class="container-item">
-               <div class="line"></div>
-               <div class="square"></div>
-                <div class="container-information">
-                 <p class="place-text">${job.company}</p>
-                 <p class="location-text" style="margin-top:-18px">${job.location}</p>
-                  <div class="date_location" style="margin-top:-22px">
-                   <h4 class="degree-text">${job.position}</h4>
-                   <p class="dates">${job.dates}</p>
-                  </div> 
-                 <p class="description" style="margin-top:-15px">${job.description}</p>
-                </div>
-            </div>`
-          );
-        });   
-        
-        // Education
-        data.education.forEach(edu => {
-          $('.education-container').append(
-            `<div class="container-item">
-               <div class="line"></div>
-               <div class="square"></div>
-                <div class="container-information">
-                  <p class="place-text">${edu.institution}</p>
-                  <h4 class="degree-text" style="margin-top: -16px">${edu.degree}</h4>
-                  <p class="dates">${edu.dates}</p>
-                  <div class="container-main-information">
-                  <p class="description">${edu.description}</p>
-                </div>
-            </div>`
-          );
-        });
-        
-        // Skills
-        data.skills.forEach(skill => {
-          $('.skills-container').append(
-            `<div class="skill">
-              <p class="skill-text">${skill.name}</p>
-              <div class="skill-bar">
-                <div class="skill-score" style="width: ${skill.score}"></div>
+      // Load About Me
+      document.querySelector('.about-main-text').textContent = data.about_me.text;
+      document.querySelector('.about-quote-text').textContent = data.about_me.quote;
+
+      // Contact Information
+      const contact = data.about_me.contact;
+      const contactContainer = document.querySelector('.contact-container');
+      contactContainer.innerHTML = `
+        <li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${contact.phone}</li>
+        <li><i class="bi bi-globe"></i>${contact.website}</li>
+        <li><i class="bi bi-geo-alt-fill"></i>${contact.address}</li>
+      `;
+
+      // References
+      const referencesContainer = document.querySelector('.references-container');
+      data.references.forEach(ref => {
+        referencesContainer.innerHTML += `
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information" style="line-height: 10px;">
+              <h4 class="degree-text">${ref.name}</h4>
+              <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${ref.address}</span></p>
+              <p class="description">Tel: ${ref.phone}</p>
+              <p class="description">Email: ${ref.email}</p>
+            </div> 
+          </div>
+        `;
+      });
+
+      // Job Experience
+      const jobContainer = document.querySelector('.job-container');
+      data.job_experience.forEach(job => {
+        jobContainer.innerHTML += `
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information">
+              <p class="place-text">${job.company}</p>
+              <p class="location-text" style="margin-top:-18px">${job.location}</p>
+              <div class="date_location" style="margin-top:-22px">
+                <h4 class="degree-text job_position">${job.position}</h4>
+                <p class="dates">${job.dates}</p>
               </div>
-            </div>`
-          );
-        });  
-        
-        // Hobbies
-        data.hobbies.forEach(hobby => {
-          $('.hobbies-container').append(
-            `<div class="skill">
-              <p class="skill-text">${hobby.name}</p>
-              <div class="skill-bar">
-                <div class="skill-score" style="width: ${hobby.score}"></div>
-              </div>
-            </div>`
-          );
-        });   
+              <p class="description experience_text">${job.description}</p>
+            </div>
+          </div>
+        `;
+      });
 
-        // Languages
-        data.languages.forEach(language => {
-          const score = parseInt(language.score);
-          const progressBarHTML = `
-            <div class="box">
-              <div class="percent">
-                <svg>
-                  <circle cx="40" cy="40" r="40"></circle>
-                  <circle cx="40" cy="40" r="40" style="stroke-dashoffset: calc(251 - (251 * ${score}) / 100);"></circle>
-                </svg>
-                <div class="num">
-                  <h1>${score}<span>%</span></h1>
-                  <h2 class="text">${language.name}</h2>
-                </div>
+      // Education
+      const educationContainer = document.querySelector('.education-container');
+      data.education.forEach(edu => {
+        educationContainer.innerHTML += `
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information">
+              <p class="place-text">${edu.institution}</p>
+              <h4 class="degree-text" style="margin-top: -16px">${edu.degree}</h4>
+              <p class="dates">${edu.dates}</p>
+              <div class="container-main-information">
+                <p class="description">${edu.description}</p>
               </div>
             </div>
-          `;
-          $('.languages-container').append(progressBarHTML);
-        });
+          </div>
+        `;
       });
-    });
-  
+
+      // Skills
+      const skillsContainer = document.querySelector('.skills-container');
+      data.skills.forEach(skill => {
+        skillsContainer.innerHTML += `
+          <div class="skill">
+            <p class="skill-text">${skill.name}</p>
+            <div class="skill-bar">
+              <div class="skill-score" style="width: ${skill.score}"></div>
+            </div>
+          </div>
+        `;
+      });
+
+      // Hobbies
+      const hobbiesContainer = document.querySelector('.hobbies-container');
+      data.hobbies.forEach(hobby => {
+        hobbiesContainer.innerHTML += `
+          <div class="skill">
+            <p class="skill-text">${hobby.name}</p>
+            <div class="skill-bar">
+              <div class="skill-score" style="width: ${hobby.score}"></div>
+            </div>
+          </div>
+        `;
+      });
+
+      // Languages
+      const languagesContainer = document.querySelector('.languages-container');
+      data.languages.forEach(language => {
+        const score = parseInt(language.score);
+        const progressBarHTML = `
+          <div class="box">
+            <div class="percent">
+              <svg>
+                <circle cx="60" cy="60" r="50"></circle>
+                <circle cx="60" cy="60" r="50" style="stroke-dashoffset: calc(314 - (314 * ${score}) / 100);"></circle>
+              </svg>
+              <div class="num">
+                <h1>${score}<span>%</span></h1>
+                <h2 class="text">${language.name}</h2>
+              </div>
+            </div>
+          </div>
+        `;
+        languagesContainer.innerHTML += progressBarHTML;
+      });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  // Call the function to load data
+  loadDataWithFetch();
+
+  // Функція для перемикання видимості кожного контейнера
+  function toggleVisibility(button, container) {
+    if (container.style.display === "none") {
+      container.style.display = "block";
+      button.innerHTML = '<i class="bi bi-caret-down-square-fill"></i>'; // Відображення заповненої іконки
+    } else {
+      container.style.display = "none";
+      button.innerHTML = '<i class="bi bi-caret-up-square-fill"></i>'; // Відображення заповненої іконки
+    }
+  }
+
+  // Функція для зміни іконок на заповнені при наведенні
+  function changeIconOnHover(button, isHovered) {
+    const icon = button.querySelector('i');
+    if (isHovered) {
+      if (icon.classList.contains('bi-caret-up-square')) {
+        icon.classList.remove('bi-caret-up-square');
+        icon.classList.add('bi-caret-up-square-fill');
+      }
+      if (icon.classList.contains('bi-caret-down-square')) {
+        icon.classList.remove('bi-caret-down-square');
+        icon.classList.add('bi-caret-down-square-fill');
+      }
+    } else {
+      if (icon.classList.contains('bi-caret-up-square-fill')) {
+        icon.classList.remove('bi-caret-up-square-fill');
+        icon.classList.add('bi-caret-up-square');
+      }
+      if (icon.classList.contains('bi-caret-down-square-fill')) {
+        icon.classList.remove('bi-caret-down-square-fill');
+        icon.classList.add('bi-caret-down-square');
+      }
+    }
+  }
+
+  // Опис секцій
+  const sections = [
+    { containerSelector: ".about_me", titleSelector: ".title", containerIndex: 0 },
+    { containerSelector: ".references-container", titleSelector: ".title", containerIndex: 1 },
+    { containerSelector: ".job-container", titleSelector: ".title", containerIndex: 2 },
+    { containerSelector: ".education-container", titleSelector: ".title", containerIndex: 3 },
+    { containerSelector: ".skills-container", titleSelector: ".title", containerIndex: 4 },
+    { containerSelector: ".hobbies-container", titleSelector: ".title", containerIndex: 5 },
+    { containerSelector: ".languages-container", titleSelector: ".title", containerIndex: 6 }
+  ];
+
+  sections.forEach(section => {
+    const container = document.querySelectorAll(section.containerSelector)[0];
+    const title = document.querySelectorAll(section.titleSelector)[section.containerIndex];
+    if (container && title) {
+      // Сховати контейнер за замовчуванням
+      container.style.display = "none";
+
+      // Створити кнопку для перемикання
+      const toggleButton = document.createElement("button");
+      toggleButton.classList.add("btn", "custom-button", "btn-sm", "ms-2");
+      toggleButton.style.cssText = `
+        background-color: #fffff;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        padding: 2px 6px;
+        cursor: pointer;
+        font-size: 20px;
+        transition: background-color 0.3s;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3); 
+        margin-bottom: 4px;
+      `;
+      title.style.cssText = `
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      `;
+      toggleButton.innerHTML = '<i class="bi bi-caret-up-square"></i>'; // Іконка за замовчуванням
+
+      // Додати кнопку поруч із заголовком
+      title.appendChild(toggleButton);
+
+      // Додати подію для перемикання видимості
+      toggleButton.addEventListener("click", function () {
+        toggleVisibility(toggleButton, container);
+      });
+
+      // Додати події для зміни іконки при наведенні
+      toggleButton.addEventListener("mouseover", function () {
+        changeIconOnHover(toggleButton, true);
+      });
+      toggleButton.addEventListener("mouseout", function () {
+        changeIconOnHover(toggleButton, false);
+      });
+    }
+  });
+});
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,57 +1,85 @@
-$(document).ready(function(){$.getJSON("data/data.json",function(i){$(".profile-name").text(i.name),$(".profile-surname").text(i.surname),$(".position-title").text(i.position),$(".about-main-text").text(i.about_me.text),$(".about-quote-text").text(i.about_me.quote);var e=i.about_me.contact;$(".contact-container").append(`<li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${e.phone}</li>
-            <li><i class="bi bi-globe"></i>${e.website}</li>
-            <li><i class="bi bi-geo-alt-fill"></i>${e.address}</li>`),i.references.forEach(i=>{$(".references-container").append(`<div class="container-item">
-              <div class="line"></div>
-              <div class="square"></div>
-              <div class="container-information" style="line-height: 10px;">
-                <h4 class="degree-text">${i.name}</h4>
-                <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${i.address}</span></p>
-                <p class="description">Tel: ${i.phone}</p>
-                <p class="description">Email: ${i.email}</p>
-              </div> 
-            </div>`)}),i.job_experience.forEach(i=>{$(".job-container").append(`<div class="container-item">
-               <div class="line"></div>
-               <div class="square"></div>
-                <div class="container-information">
-                 <p class="place-text">${i.company}</p>
-                 <p class="location-text" style="margin-top:-18px">${i.location}</p>
-                  <div class="date_location" style="margin-top:-22px">
-                   <h4 class="degree-text">${i.position}</h4>
-                   <p class="dates">${i.dates}</p>
-                  </div> 
-                 <p class="description" style="margin-top:-15px">${i.description}</p>
-                </div>
-            </div>`)}),i.education.forEach(i=>{$(".education-container").append(`<div class="container-item">
-               <div class="line"></div>
-               <div class="square"></div>
-                <div class="container-information">
-                  <p class="place-text">${i.institution}</p>
-                  <h4 class="degree-text" style="margin-top: -16px">${i.degree}</h4>
-                  <p class="dates">${i.dates}</p>
-                  <div class="container-main-information">
-                  <p class="description">${i.description}</p>
-                </div>
-            </div>`)}),i.skills.forEach(i=>{$(".skills-container").append(`<div class="skill">
-              <p class="skill-text">${i.name}</p>
-              <div class="skill-bar">
-                <div class="skill-score" style="width: ${i.score}"></div>
+document.addEventListener("DOMContentLoaded",function(){function t(e,t){e=e.querySelector("i");t?(e.classList.contains("bi-caret-up-square")&&(e.classList.remove("bi-caret-up-square"),e.classList.add("bi-caret-up-square-fill")),e.classList.contains("bi-caret-down-square")&&(e.classList.remove("bi-caret-down-square"),e.classList.add("bi-caret-down-square-fill"))):(e.classList.contains("bi-caret-up-square-fill")&&(e.classList.remove("bi-caret-up-square-fill"),e.classList.add("bi-caret-up-square")),e.classList.contains("bi-caret-down-square-fill")&&(e.classList.remove("bi-caret-down-square-fill"),e.classList.add("bi-caret-down-square")))}(async()=>{try{var e=await fetch("data/data.json");if(!e.ok)throw new Error("HTTP error! Status: "+e.status);var o=await e.json(),r=(document.querySelector(".profile-name").textContent=o.name,document.querySelector(".profile-surname").textContent=o.surname,document.querySelector(".position-title").textContent=o.position,document.querySelector(".about-main-text").textContent=o.about_me.text,document.querySelector(".about-quote-text").textContent=o.about_me.quote,o.about_me.contact);document.querySelector(".contact-container").innerHTML=`
+        <li style="font-weight: 800"><i class="bi bi-telephone-fill"></i>${r.phone}</li>
+        <li><i class="bi bi-globe"></i>${r.website}</li>
+        <li><i class="bi bi-geo-alt-fill"></i>${r.address}</li>
+      `;let t=document.querySelector(".references-container"),i=(o.references.forEach(e=>{t.innerHTML+=`
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information" style="line-height: 10px;">
+              <h4 class="degree-text">${e.name}</h4>
+              <p class="place-text"><span style="color: #dc0a0a; font-weight: 800;">${e.address}</span></p>
+              <p class="description">Tel: ${e.phone}</p>
+              <p class="description">Email: ${e.email}</p>
+            </div> 
+          </div>
+        `}),document.querySelector(".job-container")),n=(o.job_experience.forEach(e=>{i.innerHTML+=`
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information">
+              <p class="place-text">${e.company}</p>
+              <p class="location-text" style="margin-top:-18px">${e.location}</p>
+              <div class="date_location" style="margin-top:-22px">
+                <h4 class="degree-text job_position">${e.position}</h4>
+                <p class="dates">${e.dates}</p>
               </div>
-            </div>`)}),i.hobbies.forEach(i=>{$(".hobbies-container").append(`<div class="skill">
-              <p class="skill-text">${i.name}</p>
-              <div class="skill-bar">
-                <div class="skill-score" style="width: ${i.score}"></div>
-              </div>
-            </div>`)}),i.languages.forEach(i=>{var e=parseInt(i.score),e=`
-            <div class="box">
-              <div class="percent">
-                <svg>
-                  <circle cx="40" cy="40" r="40"></circle>
-                  <circle cx="40" cy="40" r="40" style="stroke-dashoffset: calc(251 - (251 * ${e}) / 100);"></circle>
-                </svg>
-                <div class="num">
-                  <h1>${e}<span>%</span></h1>
-                  <h2 class="text">${i.name}</h2>
-                </div>
+              <p class="description experience_text">${e.description}</p>
+            </div>
+          </div>
+        `}),document.querySelector(".education-container")),s=(o.education.forEach(e=>{n.innerHTML+=`
+          <div class="container-item">
+            <div class="line"></div>
+            <div class="square"></div>
+            <div class="container-information">
+              <p class="place-text">${e.institution}</p>
+              <h4 class="degree-text" style="margin-top: -16px">${e.degree}</h4>
+              <p class="dates">${e.dates}</p>
+              <div class="container-main-information">
+                <p class="description">${e.description}</p>
               </div>
             </div>
-          `;$(".languages-container").append(e)})})});
+          </div>
+        `}),document.querySelector(".skills-container")),a=(o.skills.forEach(e=>{s.innerHTML+=`
+          <div class="skill">
+            <p class="skill-text">${e.name}</p>
+            <div class="skill-bar">
+              <div class="skill-score" style="width: ${e.score}"></div>
+            </div>
+          </div>
+        `}),document.querySelector(".hobbies-container")),c=(o.hobbies.forEach(e=>{a.innerHTML+=`
+          <div class="skill">
+            <p class="skill-text">${e.name}</p>
+            <div class="skill-bar">
+              <div class="skill-score" style="width: ${e.score}"></div>
+            </div>
+          </div>
+        `}),document.querySelector(".languages-container"));o.languages.forEach(e=>{var t=parseInt(e.score),t=`
+          <div class="box">
+            <div class="percent">
+              <svg>
+                <circle cx="60" cy="60" r="50"></circle>
+                <circle cx="60" cy="60" r="50" style="stroke-dashoffset: calc(314 - (314 * ${t}) / 100);"></circle>
+              </svg>
+              <div class="num">
+                <h1>${t}<span>%</span></h1>
+                <h2 class="text">${e.name}</h2>
+              </div>
+            </div>
+          </div>
+        `;c.innerHTML+=t})}catch(e){console.error("Error fetching data:",e)}})();[{containerSelector:".about_me",titleSelector:".title",containerIndex:0},{containerSelector:".references-container",titleSelector:".title",containerIndex:1},{containerSelector:".job-container",titleSelector:".title",containerIndex:2},{containerSelector:".education-container",titleSelector:".title",containerIndex:3},{containerSelector:".skills-container",titleSelector:".title",containerIndex:4},{containerSelector:".hobbies-container",titleSelector:".title",containerIndex:5},{containerSelector:".languages-container",titleSelector:".title",containerIndex:6}].forEach(e=>{let n=document.querySelectorAll(e.containerSelector)[0];e=document.querySelectorAll(e.titleSelector)[e.containerIndex];if(n&&e){n.style.display="none";let i=document.createElement("button");i.classList.add("btn","custom-button","btn-sm","ms-2"),i.style.cssText=`
+        background-color: #fffff;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        padding: 2px 6px;
+        cursor: pointer;
+        font-size: 20px;
+        transition: background-color 0.3s;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3); 
+        margin-bottom: 4px;
+      `,e.style.cssText=`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      `,i.innerHTML='<i class="bi bi-caret-up-square"></i>',e.appendChild(i),i.addEventListener("click",function(){var e,t;e=i,"none"===(t=n).style.display?(t.style.display="block",e.innerHTML='<i class="bi bi-caret-down-square-fill"></i>'):(t.style.display="none",e.innerHTML='<i class="bi bi-caret-up-square-fill"></i>')}),i.addEventListener("mouseover",function(){t(i,!0)}),i.addEventListener("mouseout",function(){t(i,!1)})}})});
